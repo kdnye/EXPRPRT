a298fd85bcf76d2448d2cb7df9fa4a295020df1b
kdnye <david.alexander@freightservices.net>
2025-10-22 16:22:31 -0700

    chore: upgrade frontend eslint toolchain

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
@@ -9,6 +10,7 @@ export default [
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
